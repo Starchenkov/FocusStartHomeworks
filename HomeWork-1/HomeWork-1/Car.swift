5ce62122cd66bff95097cb5de97c5a83eb5280d0
Starchenkov <Starchenkov@live.ru>
2021-04-19 13:31:45 +0700

    Задание на проверку
    Задание на проверку

--- HomeWork-1/HomeWork-1/Car.swift
+++ HomeWork-1/HomeWork-1/Car.swift
@@ -5,4 +5,38 @@
 //  Created by Sergey Starchenkov on 19.04.2021.
 //
 
-import Foundation
+struct Car
+{
+    var description: String {
+        var description = "Производитель: \(self.manufacturer) \nМодель: \(self.model) \nТип кузова: \(self.body) \n"
+
+        if let yearOfIssue = self.yearOfIssue {
+            description += "Год выпуска: \(yearOfIssue) \n"
+        }else {
+            description += "Год выпуска: - \n"
+        }
+        
+        if let carNumber = self.carNumber, carNumber != "" {
+            description += "Гос номер: \(carNumber) \n"
+        }
+        return description
+    }
+    
+    enum Body: Int {
+        
+        case sedan = 1
+        case jeep
+        case suv
+        
+        static func printAllCases() {
+            print("1 - sedan, 2 - jeep, 3 - suv")
+        }
+    }
+    
+    let manufacturer: String
+    let model: String
+    let body: Body
+    let yearOfIssue: Int?
+    let carNumber: String?
+
+}
--- HomeWork-1/HomeWork-1/main.swift
+++ HomeWork-1/HomeWork-1/main.swift
@@ -5,7 +5,13 @@
 //  Created by Sergey Starchenkov on 19.04.2021.
 //
 
-import Foundation
+let cars: [Car] = [
+    Car(manufacturer: "Toyota", model: "Camry", body: .sedan, yearOfIssue: 2019, carNumber: nil),
+    Car(manufacturer: "Jeep", model: "Cherokee", body: .jeep, yearOfIssue: 2013, carNumber: "Е234ЕЕ99"),
+    Car(manufacturer: "Toyota", model: "Rav4", body: .suv, yearOfIssue: 2018, carNumber: nil)]
 
-print("Hello, World!")
+let mainApp = MainApp(cars: cars)
+mainApp.start()
+
+
 
